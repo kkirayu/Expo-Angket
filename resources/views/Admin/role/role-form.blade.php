ccf325f742d22dfdb7dbe5506483b27758d2ae4f
Fathur Rahman <sergio.utina15@gmail.com>
2024-01-30 08:43:39 +0700

    multiple insert role

--- resources/views/Admin/role/role-form.blade.php
+++ resources/views/Admin/role/role-form.blade.php
@@ -18,27 +18,33 @@
                                     @csrf
                                     @if (isset($edit))
                                         @method('PUT')
+                                        <input type="hidden" value="{{ $judul->slug }}" name="slug" />
                                         <input type="hidden" value="{{ $edit->id }}" name="id" />
                                     @endif
-                                    <input type="hidden" value="{{ old('role', isset($edit) ? $edit->role : $acara) }}" name="acara" />
-                                    <div class="row mb-3">
-                                        <div class="col-sm-3">
-                                            <h6 class="mb-0">Nama Role</h6>
-                                        </div>
-                                        <div class="form-group col-sm-9 text-secondary">
-                                            <input type="text" name="role" class="form-control"
-                                                value="{{ old('role', isset($edit) ? $edit->role : '') }}"
-                                                placeholder="Ex: Pengunjung" />
+                                    <input type="hidden" value="{{ old('role', isset($edit) ? $edit->role : $acara) }}"
+                                        name="acara" />
+                                    <div id="divRole">
+                                        <div class="row mb-3">
+                                            <div class="col-sm-3">
+                                                <h6 class="mb-0">Nama Role</h6>
+                                            </div>
+                                            <div class="form-group col-sm-9 text-secondary">
+                                                <input type="text" name="req[0][role]" class="form-control"
+                                                    value="{{ old('role', isset($edit) ? $edit->role : '') }}"
+                                                    placeholder="Ex: Pengunjung" />
                                                 @error('role')
                                                     <span class="text-danger">{{ $message }}</span>
                                                 @enderror
+                                            </div>
                                         </div>
                                     </div>
                                     <div class="row">
-                                        <div class="col-sm-3"></div>
-                                        <div class="col-sm-9 text-secondary" style="text-align: right;">
+                                        <div class="col-sm-7"><p {{ isset($edit) ? 'hidden' : '' }}>*Untuk menginputkan beberapa role klik tombol <b>Tambah Role</b></p></div>
+                                        <div class="col-sm-5 text-secondary" style="text-align: right;">
+                                            <button type="button" id="tambahRole" class="btn btn-secondary" {{ isset($edit) ? 'hidden' : '' }}>Tambah
+                                                Role</button>
                                             <input type="submit" class="btn btn-success px-4"
-                                                value="{{ isset($edit) ? 'Ubah Role' : 'Tambah Role' }}" />
+                                                value="Submit Role" />
                                         </div>
                                     </div>
                                 </form>
@@ -50,5 +56,30 @@
             </div>
         </div>
     </div>
-
+    @push('custom-script')
+    <script>
+        var i = 0;
+        var u = 1;
+        $("#tambahRole").click(function() {
+            ++i;
+            ++u;
+            $('#divRole').append(`
+            <hr>
+            <div class="row mb-3">
+                <div class="col-sm-3">
+                    <h6 class="mb-0">Nama Role `+ u +`</h6>
+                </div>
+                <div class="form-group col-sm-9 text-secondary">
+                    <input type="text" name="req[`+ i +`][role]" class="form-control"
+                        value="{{ old('role', isset($edit) ? $edit->role : '') }}"
+                        placeholder="Ex: Pengunjung" />
+                    @error('role')
+                        <span class="text-danger">{{ $message }}</span>
+                    @enderror
+                </div>
+            </div>
+        `);
+        });
+    </script>
+    @endpush
 @endsection
